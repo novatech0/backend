d821f7cd4aff31ed0497a7b0fa61c26f41a48194
Piero Delgado <112528610+PieroD04@users.noreply.github.com>
2025-06-05 13:08:08 -0500

    feat: change table names

--- src/main/java/com/agrotech/api/shared/infrastructure/persistence/jpa/configuration/strategy/SnakeCaseTableNamingStrategy.java
+++ src/main/java/com/agrotech/api/shared/infrastructure/persistence/jpa/configuration/strategy/SnakeCaseTableNamingStrategy.java
@@ -4,33 +4,37 @@ import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 
-import static io.github.encryptorcode.pluralize.Pluralize.pluralize;
-public class SnakeCaseWithPluralizedTablePhysicalNamingStrategy implements PhysicalNamingStrategy {
+public class SnakeCaseTableNamingStrategy implements PhysicalNamingStrategy {
     @Override
     public Identifier toPhysicalCatalogName(Identifier identifier,
                                             JdbcEnvironment jdbcEnvironment) {
         return this.toSnakeCase(identifier);
     }
+
     @Override
     public Identifier toPhysicalSchemaName(Identifier identifier,
                                            JdbcEnvironment jdbcEnvironment) {
         return this.toSnakeCase(identifier);
     }
+
     @Override
     public Identifier toPhysicalTableName(Identifier identifier,
                                           JdbcEnvironment jdbcEnvironment) {
-        return this.toSnakeCase(this.toPlural(identifier));
+        return this.toSnakeCase(identifier);
     }
+
     @Override
     public Identifier toPhysicalSequenceName(Identifier identifier,
                                              JdbcEnvironment jdbcEnvironment) {
         return this.toSnakeCase(identifier);
     }
+
     @Override
     public Identifier toPhysicalColumnName(Identifier identifier,
                                            JdbcEnvironment jdbcEnvironment) {
         return this.toSnakeCase(identifier);
     }
+
     private Identifier toSnakeCase(final Identifier identifier) {
         if (identifier == null) {
             return null;
@@ -40,10 +44,6 @@ public class SnakeCaseWithPluralizedTablePhysicalNamingStrategy implements Physi
         final String newName = identifier.getText()
                 .replaceAll(regex, replacement)
                 .toLowerCase();
-        return Identifier.toIdentifier(newName);
-    }
-    private Identifier toPlural(final Identifier identifier) {
-        final String newName = pluralize(identifier.getText());
         return Identifier.toIdentifier(newName);
     }
 }
